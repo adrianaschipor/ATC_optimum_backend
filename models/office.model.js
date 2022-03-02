7be1683c1a326cc78ca5e90659287b1873d67dfb
Adriana <adrianaschipor7@gmail.com>
2022-03-02 15:51:54 +0200

    Created office model

---
+++ models/office.model.js
@@ -0,0 +1,52 @@
+const Sequelize = require("sequelize");
+const db = require("../config/database.config");
+const building_model = require("./building.model");
+const user_model = require("./user.model");
+
+const office = db.define("Office", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  floorNo: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  totalDesksCount: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  usableDesksCount: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  width: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+  },
+  length: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+  },
+  buildingId: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: building_model,
+      key: "id",
+    },
+    onDelete: "cascade",
+    allowNull: false,
+  },
+  officeAdminId: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: user_model,
+      key: "id",
+    },
+    onDelete: "cascade",
+    allowNull: true,
+  },
+});
+
+office.sync({ alter: true });
+module.exports = office;
