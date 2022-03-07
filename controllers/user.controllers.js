3032d237f055a4f8d0b4e920a4082be5670bdfda
Adriana <adrianaschipor7@gmail.com>
2022-03-07 16:43:36 +0200

    Added Update operation for User

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -91,3 +91,107 @@ exports.create = async (req, res) => {
     return res.status(500).send({ message: err.message });
   }
 };
+
+//Update existing user
+exports.update = async (req, res) => {
+  try {
+    const currentUser = await User.findOne({
+      where: { id: req.params.userId },
+    });
+    if (!currentUser) return res.status(404).send("User not found.");
+
+    const {
+      firstname,
+      lastname,
+      email,
+      password,
+      role,
+      gender,
+      birthday,
+      nationality,
+      active,
+    } = req.body;
+
+    //VALIDATIONS
+    //name
+    if (firstname == null || firstname.length > 30) {
+      return res.status(400).send({ message: "Invalid first name." });
+    }
+    if (lastname == null || lastname.length > 30) {
+      return res.status(400).send({ message: "Invalid last name." });
+    }
+    //email
+    const emailExists = await User.findOne({
+      where: { email: email },
+    });
+    if (emailExists && emailExists != currentUser.email)
+      return res.status(400).json({ msg: "This email is already used" });
+    var regex = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
+    if (!regex.test(email) || email.length > 50) {
+      return res.status(400).send({ message: "Invalid email format" });
+    }
+    //password
+    var regex =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+    if (!regex.test(password)) {
+      return res.status(400).json({
+        msg: "Invalid password. Minimum 8 characters, at least one letter, one number and one special character",
+      });
+    }
+    //role
+    if (role != "Admin" && role != "Office Admin" && role != "Employee")
+      return res.status(400).send({
+        message:
+          "Invalid role. Available roles are: Admin, Office Admin and Employee.",
+      });
+    //gender
+    if (gender != "Male" && gender != "Female" && gender != "Other")
+      return res.status(400).send({
+        message: "Invalid gender.",
+      });
+    //birthday
+    if (birthday != null) {
+      let birthdayAsDate = new Date(birthday);
+      let year = birthdayAsDate.getFullYear;
+      if (
+        birthdayAsDate == NaN ||
+        year < 1900 ||
+        year > new Date().getFullYear() + 1
+      )
+        return res.status(400).send({
+          message: "Invalid birthday",
+        });
+    }
+    //nationality
+    if (nationality != null) {
+      if (nationality.length > 30)
+        return res.status(400).send({
+          message: "Invalid nationality.",
+        });
+    }
+
+    //treat activating/deactivating situation
+
+    const updatedUser = {
+      firstname,
+      lastname,
+      email,
+      password,
+      role,
+      gender,
+      birthday,
+      nationality,
+      active,
+    };
+
+    if (
+      (await User.update(updatedUser, {
+        where: { id: req.params.userId },
+      })) != 1
+    )
+      return res.status(404).send("Couldn't update User!");
+    return res.status(200).send("User updated successfully !");
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+};
