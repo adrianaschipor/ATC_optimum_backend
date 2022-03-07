0a9501504ee58e105609a87c8b45ad1a0e1a0592
Adriana <adrianaschipor7@gmail.com>
2022-03-07 17:45:18 +0200

    Added Update for Building

--- controllers/building.controllers.js
+++ controllers/building.controllers.js
@@ -37,3 +37,46 @@ exports.create = async (req, res) => {
 };
 
 //Update existing Building
+exports.update = async (req, res) => {
+  try {
+    const { name, address, floorsCount } = req.body;
+
+    const currentBuilding = await Building.findOne({
+      where: { id: req.params.buildingId },
+    });
+    if (!currentBuilding) return res.status(404).send("Building not found.");
+
+    //VALIDATIONS
+    //name
+    if (name == null || name.length > 30)
+      return res.status(400).send({ message: "Invalid Name." });
+    //address
+    if (address == null || address.length > 50)
+      return res.status(400).send({ message: "Invalid Address." });
+    const existingBuilding = await Building.findOne({
+      where: { address: address },
+    });
+    if (existingBuilding && address != currentBuilding.address)
+      return res.status(400).send({
+        message: "Invalid Address. There's already a building at this address.",
+      });
+    //floorsCount
+    if (floorsCount == null || floorsCount < 1 || floorsCount > 150)
+      return res.status(400).send({ message: "Invalid Floors Count." });
+
+    const updatedBuilding = {
+      name,
+      address,
+      floorsCount,
+    };
+    if (
+      (await Building.update(updatedBuilding, {
+        where: { id: req.params.buildingId },
+      })) != 1
+    )
+      return res.status(404).send("Couldn't update Building !");
+    return res.status(200).send("Building successfully updated !");
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+};
