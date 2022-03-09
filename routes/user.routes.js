e1df1ab43f5755a20f922392bb9477468697ae5c
Adriana <adrianaschipor7@gmail.com>
2022-03-09 18:45:04 +0200

    Added auth middlewares to routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,21 +1,26 @@
 module.exports = (app) => {
   const User = require("../controllers/user.controllers");
+  const auth = require("../middlewares/authJwt");
+  const authAdmin = require("../middlewares/authAdmin");
+  const authOfficeAdmin = require("../middlewares/authOfficeAdmin");
+  const authAllAdminTypes = require("../middlewares/authAllAdminTypes");
+  const authAllUsers = require("../middlewares/authAllUsers");
 
   //Create new User
-  app.post("/user", User.create);
+  app.post("/user", auth, authAdmin, User.create);
 
   //Update existing User
-  app.put("/user/:userId", User.update);
+  app.put("/user/:userId", auth, authAdmin, User.update);
 
   //Get all users
-  app.get("/allUsers", User.findAll);
+  app.get("/allUsers", auth, authAllAdminTypes, User.findAll);
 
   //Get all office admins
-  app.get("/allOfficeAdmins", User.findAllOfficeAdmins);
+  app.get("/allOfficeAdmins", auth, authAdmin, User.findAllOfficeAdmins);
 
   //Get all users from an Office Admin offices
-  //   app.get("/allUsersOfOfficeAdmin", User.findAllUsersOfOfficeAdmin);
+  //   app.get("/allUsersOfOfficeAdmin", auth, authOfficeAdmin,User.findAllUsersOfOfficeAdmin);
 
   //Get user status
-  //app.get("/user/:name", User.findOne);
+  //app.get("/user/:name", auth, authAllUsers,User.findAllByName);
 };
