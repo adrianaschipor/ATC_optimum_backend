ea98792aaeeab2fd52a941d2540a1f00e907fbef
Adriana <adrianaschipor7@gmail.com>
2022-03-09 18:43:47 +0200

    Implemented middleware that allows only admins and  office admins to perform actions

---
+++ middlewares/authAllAdminTypes.js
@@ -0,0 +1,15 @@
+// middleware to allow actions only for Admins and Office Admins
+const authAllAdminTypes = async (req, res, next) => {
+  try {
+    if (req.user.role != "Office Admin" && req.user.role != "Admin") {
+      return res.status(403).json({
+        msg: "Forbidden: only an Admin or an Office Admin can perform this action",
+      });
+    }
+    return next();
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
+  }
+};
+
+module.exports = authAllAdminTypes;
