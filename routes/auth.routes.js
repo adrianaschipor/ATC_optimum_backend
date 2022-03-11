5cbf3b632e1b676ebf05442319b7143fcf217a58
Adriana <adrianaschipor7@gmail.com>
2022-03-11 11:44:39 +0200

    Added comments for auth routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,9 @@
 module.exports = (app) => {
   const Auth = require("../controllers/auth.controller");
 
+  // Login endpoint
   app.post("/login", Auth.login);
+
+  // Refresh token endpoint
   app.post("/refresh-token", Auth.refreshToken);
 };
