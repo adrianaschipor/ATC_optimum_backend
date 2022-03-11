44e386ceb5abe53a898a27f208fc0c1ad3d7d934
Adriana <adrianaschipor7@gmail.com>
2022-03-11 16:37:42 +0200

    Added necessary middlewares for office routes

--- routes/office.routes.js
+++ routes/office.routes.js
@@ -1,21 +1,51 @@
-module.exports = (app) => {
-  const Office = require("../controllers/office.controllers");
+const Office = require("../controllers/office.controllers");
 
+const auth = require("../middlewares/authJwt");
+const authAdmin = require("../middlewares/authAdmin");
+const authAllAdminTypes = require("../middlewares/authAllAdminTypes");
+const authAllUsers = require("../middlewares/authAllUsers");
+const activeAccount = require("../middlewares/activeAccount");
+
+module.exports = (app) => {
   //Create new Office
-  app.post("/office", Office.create);
+  app.post("/office", auth, authAdmin, activeAccount, Office.create);
 
   //Update existing Office
-  app.put("/office/:officeId", Office.update);
+  app.put("/office/:officeId", auth, authAdmin, activeAccount, Office.update);
 
   //Delete existing Office
-  app.delete("/office/:officeId", Office.delete);
+  app.delete(
+    "/office/:officeId",
+    auth,
+    authAdmin,
+    activeAccount,
+    Office.delete
+  );
 
   //Get all Offices
-  app.get("/allOffices", Office.findAll);
+  app.get(
+    "/allOffices",
+    auth,
+    authAllAdminTypes,
+    activeAccount,
+    Office.findAll
+  );
 
   //Get all Offices from a building
-  app.get("/allOffices/:buildingId", Office.findAllFromBuilding);
+  app.get(
+    "/allOffices/:buildingId",
+    auth,
+    authAllAdminTypes,
+    activeAccount,
+    Office.findAllFromBuilding
+  );
 
-  //Get  Office Status
-  app.get("/office/:officeId", Office.findOne);
+  //Get Office Status
+  app.get(
+    "/office/:officeId",
+    auth,
+    authAllUsers,
+    activeAccount,
+    Office.findOne
+  );
 };
