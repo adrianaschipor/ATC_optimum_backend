c06e8586dfef8d478871cbed2c78e07aa0198133
Adriana <adrianaschipor7@gmail.com>
2022-03-14 15:07:48 +0200

    Implemented Desk Approval Feature

--- controllers/deskReq.controllers.js
+++ controllers/deskReq.controllers.js
@@ -2,6 +2,7 @@ const DeskReq = require("../models/deskReq.model");
 const User = require("../models/user.model");
 const Office = require("../models/office.model");
 const Desk = require("../models/desk.model");
+const RemoteReq = require("../models/remoteReq.model");
 
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
@@ -53,6 +54,130 @@ exports.create = async (req, res) => {
 
     const deskReq = await DeskReq.create(newDeskReq);
     return res.status(201).send(deskReq.id.toString());
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+};
+
+// Approve/Reject a request
+// Approve / Reject a request
+exports.approval = async (req, res) => {
+  try {
+    const { status, rejectReason } = req.body;
+
+    const theReq = await DeskReq.findOne({
+      where: { id: req.params.deskReqId },
+    });
+    if (!theReq) return res.status(404).send("Desk Request not found.");
+
+    //VALIDATIONS
+    //status
+    if (status != "Approved" && status != "Rejected")
+      return res.status(404).send("Invalid Status");
+    //rejectReason
+    if (status === "Rejected") {
+      if (!rejectReason || rejectReason === "")
+        return res.status(400).send({
+          message:
+            "Invalid Rejection Reason: You should provide a reason for your rejection",
+        });
+      if (rejectReason.length > 2000)
+        return res.status(400).send({
+          message: "Invalid Rejection Reason: Your reason is too long",
+        });
+    }
+    // check if the office is administrated by this Office Admin
+    const office = await Office.findOne({
+      where: { id: theReq.officeId, officeAdminId: req.user.id },
+    });
+    if (!office)
+      return res.status(403).json({
+        msg: "Forbidden: This office is not under your administration",
+      });
+
+    // Treating desk approvement consequences
+    if (status === "Approved") {
+      // check again if there are any free desks in the office (maybe they got occupied meanwhile)
+      const freeDesk = await Desk.findOne({
+        where: { officeId: theReq.officeId, usable: true, userId: null },
+      });
+      if (!freeDesk)
+        return res.status(400).send({
+          message:
+            "You cannot approve this request: This Offce has no free desks",
+        });
+      // previous desk occupied by user becomes free
+      let previousDesk = await Desk.findOne({
+        where: { userId: theReq.userId },
+      });
+      if (previousDesk) {
+        previousDesk.userId = null;
+        await previousDesk.save();
+      }
+      // user is assigned to the free desk from the new office
+      freeDesk.userId = theReq.userId;
+      await freeDesk.save();
+      // if user is fully working remote, percentage goes down to 0
+      const currentDate = new Date();
+      const currentYear = currentDate.getFullYear();
+      const currentMonth = currentDate.getMonth() + 1; //the +1 converts the month from digital (0-11) to normal.
+      const working100Remote = await RemoteReq.findOne({
+        where: {
+          [Op.and]: [
+            { userId: theReq.userId },
+            { status: "Approved" },
+            { year: currentYear },
+            { month: currentMonth },
+            { percentage: 100 },
+          ],
+        },
+      });
+      if (working100Remote) {
+        working100Remote.percentage = 0;
+        await working100Remote.save();
+      }
+    }
+
+    theReq.status = status;
+    theReq.rejectReason = rejectReason;
+    await theReq.save();
+    return res.status(200).send("Desk Request successfully approved/rejected");
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+};
+
+// get all pending requests for desks from an Office Admin offices
+exports.findAllPending = async (req, res) => {
+  try {
+    // get the offices that are under this Office Admin administration
+    const offices = await Office.findAll({
+      attributes: ["id"],
+      where: { officeAdminId: req.user.id },
+    });
+
+    //Select only those requests for offices that are under this Office Admin's administration
+    let allRequests = [];
+    for (const office of offices) {
+      let requestsForOffice = [];
+      requestsForOffice = await DeskReq.findAll({
+        where: { status: "Pending", officeId: office.id },
+      });
+      if (requestsForOffice)
+        allRequests = allRequests.concat(requestsForOffice);
+    }
+    return res.status(200).send(allRequests);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+};
+
+// get all user's requests
+exports.findAll = async (req, res) => {
+  try {
+    let allRequests = [];
+    allRequests = await DeskReq.findAll({ where: { userId: req.user.id } });
+    return res.status(200).send(allRequests);
   } catch (err) {
     return res.status(500).send({ message: err.message });
   }
--- routes/deskReq.routes.js
+++ routes/deskReq.routes.js
@@ -1,9 +1,35 @@
 const auth = require("../middlewares/authJwt");
-const authAllAdminTypes = require("../middlewares/authAllAdminTypes");
+const authOffceAdmin = require("../middlewares/authOfficeAdmin");
 const authEmployee = require("../middlewares/authEmployee");
 const activeAccount = require("../middlewares/activeAccount");
 
 const DeskReq = require("../controllers/deskReq.controllers");
 module.exports = (app) => {
+  // Endpoint for submiting a desk request
+  // Only an authenticated Employee with an active account can perform this
   app.post("/deskReq", auth, authEmployee, activeAccount, DeskReq.create);
+
+  // Endpoint for aproving/rjecting a desk request
+  // Only an authenticated Office Admin with an active account can perform this
+  app.put(
+    "/deskReq/:deskReqId",
+    auth,
+    authOffceAdmin,
+    activeAccount,
+    DeskReq.approval
+  );
+
+  // Endpoint for getting a list of all pending request for an office admin offices
+  // Only an authenticated Office Admin with an active account can perform this
+  app.get(
+    "/pendingDeskReq",
+    auth,
+    authOffceAdmin,
+    activeAccount,
+    DeskReq.findAllPending
+  );
+
+  // Endpoint for getting a list of all user's request
+  // Only an authenticated Employee with an active account can perform this
+  app.get("/allDeskReq", auth, authEmployee, activeAccount, DeskReq.findAll);
 };
